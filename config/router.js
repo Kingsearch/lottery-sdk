490fb22d2dc6c15434d387c76889ddf68b9ea5b8
用思想的力量改变世界 <24767716@qq.com>
2017-12-01 20:01:41 +0800

    'update'

--- config/router.js
+++ config/router.js
@@ -1,8 +1,12 @@
 const { Router, Route } = require('dva/router');
 const Home = require('../src/routes/home');
 
-const router =  <Route path="">
-                    <Route path="/home" component={Home} />
-                </Route>
+const router = function(){
+    return(
+        <Route path="">
+            <Route path="/home" component={Home} />
+        </Route>
+    )
+} 
 
 module.exports = router;
